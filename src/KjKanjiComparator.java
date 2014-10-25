2549232b113a3308089dca95daea86abdbc05bac
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-10-25 09:11:02 +0900

    Revert "no message"
    This reverts commit 4991c1a0536ccc0937d0b637a1722f695657442a.

--- src/KjKanjiComparator.java
+++ src/KjKanjiComparator.java
@@ -144,7 +144,7 @@ public class KjKanjiComparator extends JDialog {
 		}
 	}
 
-	public double compareKanji(ArrayList<KjStroke> word1, ArrayList<KjStroke> word2) {
+	protected double compareKanji(ArrayList<KjStroke> word1, ArrayList<KjStroke> word2) {
 		double difference = 0;
 		
 		if (word1.size()!=word2.size()){
